78c2e9034903cd90dbfe3b11846b0f80bcc42174
Hao-Tse <maxwuhz0624@gmail.com>
2019-01-22 16:20:04 -0800

    Auto stash before merge of "Hao-Tse" and "origin/master"

---
+++ 2DMobileTest/Assets/Scripts/SceneManager2.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager2: MonoBehaviour {
+
+	 public static SceneManager2 instance = null;
+	public GameObject bugobject;
+	private Vector3 range; 
+	private Vector3 range1; 
+
+
+	private static bool winningstate = false;
+
+	public List<GameObject> bug = new List<GameObject>();
+
+
+	void Start () {
+		range = new Vector3(Random.Range(2,6),Random.Range(-2.6f,2.6f),-5f);
+		range1 = new Vector3(Random.Range(2,6),Random.Range(-2.6f,2.6f),-5f);
+
+		Invoke("instantiateobject", 1.0f);
+		
+		Invoke("checkifwin", 6);
+	}
+	
+	void Update () {
+		
+	}
+
+	void Awake()
+	{
+            if (instance == null)
+                {
+                instance = this;
+				}
+            else if (instance != this)
+                {
+                Destroy(gameObject);    
+				}
+            DontDestroyOnLoad(gameObject);
+
+	}
+
+	void instantiateobject()
+	{
+		GameObject current = Instantiate(bugobject,range,Quaternion.identity);
+  		bug.Add(current);
+		current = Instantiate(bugobject,range1,Quaternion.identity);
+  		bug.Add(current);
+	}
+
+	public bool checkifwin()
+	{
+		if (bug.Count == 0)
+		{
+			winningstate = true;
+			print(true);
+			print(bug.Count);
+			return true;
+		}else
+		{
+			print(bug.Count);
+			winningstate = false;
+			print(false);
+			return false;
+		}
+	}
+
+	public static bool getwinningstate()
+	{
+		return winningstate;
+	}
+}
