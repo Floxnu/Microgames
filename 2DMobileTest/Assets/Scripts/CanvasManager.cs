7b6f3a81936e79a3d3da5f109994ad58b52f9a05
Unknown <juanjvalenciagd@gmail.com>
2019-01-22 16:09:02 -0800

    Added between game scenes, timer first pass and camera changes.

---
+++ 2DMobileTest/Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour {
+
+	public static CanvasManager instance;
+	public Image canvasBackground;
+	public Slider[] hearts;
+	public Text scoreText;
+	private Slider heartToLose;
+	private int gameTypeInt;
+	public CanvasGroup scoreHeartsGroup;
+	public CanvasGroup gameTypeGroup;
+	public CanvasGroup touchGroup;
+	public CanvasGroup tiltGroup;
+
+
+	private void Awake() {
+		if(instance != null){
+			Destroy(this);
+		}
+		instance = this;
+		DontDestroyOnLoad(this);
+	}
+
+	private void Start() {
+		ScoreFadeOut();
+		ShowTouch();
+	}
+
+	public void SetScoreText(string text){
+		scoreText.text = text;
+	}
+
+	public void LooseHeart(int heartNumber){
+
+		heartToLose = hearts[heartNumber];
+		StartCoroutine(ReduceHeart());
+
+	}
+	public void ShowTilt(){
+		tiltGroup.alpha = 1;
+		touchGroup.alpha = 0;
+		GameTypeFadeIn();
+		canvasBackground.CrossFadeAlpha(0, 2, false);
+
+	}
+	public void ShowTouch(){
+		tiltGroup.alpha = 0;
+		touchGroup.alpha = 1;
+		GameTypeFadeIn();
+
+
+	}
+	public void ScoreFadeOut(){
+
+		scoreHeartsGroup.alpha = 1;
+		StartCoroutine(FadeOutScore());
+
+	}
+	public void ScoreFadeIn(){
+
+		scoreHeartsGroup.alpha = 0;
+		StartCoroutine(FadeInScore());
+	
+	}
+	public void GameTypeFadeIn(){
+
+		gameTypeGroup.alpha = 0;
+		StartCoroutine(FadeInGameType());
+
+	}
+	public void GameTypeFadeOut(){
+
+		gameTypeGroup.alpha = 1;
+		StartCoroutine(FadeOutGameType());
+
+	}
+
+	public void FadeOutBackground(){
+		canvasBackground.CrossFadeAlpha(0, 1, false);
+
+	}
+
+
+	public void FadeInBackground(){
+
+		canvasBackground.CrossFadeAlpha(1, 1, false);
+
+	}
+
+	IEnumerator ReduceHeart(){
+
+		while(heartToLose.value > 0){
+			heartToLose.value -= 1;
+			yield return null;
+		}
+		
+	}
+	IEnumerator FadeOutScore(){
+
+
+		while(scoreHeartsGroup.alpha > 0){
+			scoreHeartsGroup.alpha -= 0.05f;
+			yield return null;
+		}
+
+	}
+	IEnumerator FadeInScore(){
+
+		while(scoreHeartsGroup.alpha < 1){
+			scoreHeartsGroup.alpha += 0.05f;
+			yield return null;
+		}
+
+	}
+	IEnumerator FadeInGameType(){
+
+		while(gameTypeGroup.alpha < 1){
+
+			gameTypeGroup.alpha += 0.05f;
+			yield return null;
+
+		}
+
+		yield return new WaitForSeconds(2);
+
+		FadeOutBackground();
+
+		StartCoroutine(FadeOutGameType());
+	}
+	IEnumerator FadeOutGameType(){
+
+		while(gameTypeGroup.alpha > 0){
+
+			gameTypeGroup.alpha -= 0.05f;
+			yield return null;
+
+		}
+
+	}
+
+}
---
+++ 2DMobileTest/Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+
+	private int livesRemaining;
+
+	public int currentScore;
+
+	public int gameDificulty;
+
+	public enum GameType{
+		TOUCH,
+		TILT
+	}
+
+	public GameType currentGameType;
+
+	public delegate void StartAction();
+	public static event StartAction OnGameStart;
+
+	private void Awake() {
+		if(instance != null){
+			Destroy(this);
+		}
+		instance = this;
+		DontDestroyOnLoad(this);
+		DontDestroyOnLoad(gameObject);
+	}
+
+	private void Start() {
+		CanvasManager.instance.SetScoreText(currentScore.ToString());
+		StartCoroutine(StartNextGame());
+		
+	}
+
+	IEnumerator StartNextGame(){
+		yield return new WaitForSeconds(2);
+
+		SceneManager.LoadScene(2, LoadSceneMode.Additive);
+		SceneManager.LoadScene(3, LoadSceneMode.Additive);
+
+		yield return new WaitForSeconds(1);
+
+	}
+}
