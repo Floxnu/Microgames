beb821508ae4a2bf69b320adff8d9665061ed98a
Unknown <juanjvalenciagd@gmail.com>
2019-01-25 11:08:56 -0800

    Finished Paths, RollBall, and updated the Transition Scene.

---
+++ 2DMobileTest/Assets/Scripts/RollBall/RollBall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollBall : MonoBehaviour
+{
+   Rigidbody2D rbRef;
+   bool canMove;
+
+   private void Awake() {
+       rbRef= GetComponent<Rigidbody2D>();
+       GameManager.OnGameStart+= CustomStart;
+   }
+
+   void CustomStart(){
+       canMove = true;
+
+        GameManager.OnGameStart -= CustomStart;
+   }
+
+   private void Update() {
+
+       if(canMove){
+            Vector3 tiltInput = Input.acceleration;
+
+            tiltInput = Quaternion.Euler(90,0, -90) * tiltInput;
+
+            rbRef.AddForce(new Vector2(-tiltInput.z, tiltInput.x + .4f) * 10);
+       }
+   }
+}
